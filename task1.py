d8fc652be1e35d5e900472385825181fc9c9c007
DmitriiMurich <murichdim@gmail.com>
2024-09-12 12:40:07 +0300

    changed project structure

--- task1.py
+++ task1.py
@@ -1,6 +1,6 @@
 # run
 from services.pdf_service import extract_all_info_from_pdf
 
-pdf_path = 'pdf_file/test_task.pdf'
+pdf_path = 'reference_file/test_task.pdf'
 pdf_info = extract_all_info_from_pdf(pdf_path)
 print(pdf_info)
