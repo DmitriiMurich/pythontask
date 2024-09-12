3d1c4c2a41237447655ade5975555428ef09ad6b
DmitriiMurich <murichdim@gmail.com>
2024-09-12 12:25:01 +0300

    add test file

--- task1.py
+++ task1.py
@@ -1,6 +1,6 @@
 # run
 from services.pdf_service import extract_all_info_from_pdf
 
-pdf_path = 'test_task.pdf'
+pdf_path = 'pdf_file/test_task.pdf'
 pdf_info = extract_all_info_from_pdf(pdf_path)
 print(pdf_info)
